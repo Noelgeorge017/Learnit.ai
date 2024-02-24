8fdf9bc6090951d8c089ef189311abb9580fe023
NzM <nazimfilzer@gmail.com>
2024-02-25 01:49:07 +0530

    Nav Bar

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -16,7 +16,7 @@ if(!session?.user) {
   return (
     <div className="flex flex-col items-start max-w-xl px-8 mx-auto my-16 sm:px-0">
       <h1 className="self-center text-3xl font-bold text-center sm:text-6xl">
-        Learning Journey
+       Begin Your Journey
       </h1>
       <div className="flex p-4 mt-5 border-none bg-secondary">
         <InfoIcon className="w-12 h-12 mr-3 text-blue-400" />
